6bfe7db4e64e743945d68651062cd659125c3016
antonkurin <antonkurin1@gmail.com>
2022-01-19 14:48:04 +0300

    HOMEWORK 19.01.22

---
+++ Files.hpp
@@ -0,0 +1,6 @@
+#ifndef Files_hpp
+#define Files_hpp
+
+#include <stdio.h>
+
+#endif /* Files_hpp */
--- InheritanceCplusplus/main.cpp
+++ InheritanceCplusplus/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
 
 using std::string;
 using std::cout;
@@ -65,19 +67,70 @@ public:
         set_first_name(first_name);
         set_last_name(last_name);
         set_age(age);
-        cout << "\nHuman Constructor: \t" << this << endl;
+       // cout << "\nHuman Constructor: \t" << this << endl;
     }
-    ~Human()
+    virtual ~Human()
     {
-        cout << "Human Destructor: \t" << this << endl;
+        //cout << "Human Destructor: \t" << this << endl;
     }
     // Methods:
-    
-    void print() const
+    virtual std::ostream& print(std::ostream& os) const
     {
-        cout << "Name: " << first_name << " " << last_name << " " << "\nAge: " << age << endl;
+        os << "Name: ";
+        os.width(5);
+        os << std::left;
+        os << last_name << " ";
+        os.width(5);
+        os << first_name << " ";
+        os << "Age: ";
+        os.width(3);
+        os << age << "years ";
+        return os;
+    }
+    virtual std::ofstream& print(std::ofstream& os)const
+    {
+        os.width(5);
+        os << std::left;
+        os << last_name;
+        os.width(10);
+        os << first_name;
+        os.width(10);
+        os << age;
+        return os;
+    }
+    virtual std::istream& input(std::istream& is)
+    {
+        cout << "Last name:  ";
+        is >> last_name;
+        cout << "First name: ";
+        is >> first_name;
+        cout << "Age:        ";
+        is >> age;
+        return is;
+    }
+    virtual std::ifstream& scan(std::ifstream& is)
+    {
+        is >> last_name >> first_name >> age;
+        return is;
     }
 };
+
+std::ostream& operator<<(std::ostream& os, const Human& obj)
+{
+    return obj.print(os);
+}
+std::istream& operator>>(std::istream& is, Human& obj)
+{
+    return obj.input(is);
+}
+std::ofstream& operator<<(std::ofstream& os, const Human& obj)
+{
+    return obj.print(os);
+}
+std::ifstream& operator>>(std::ifstream& is, Human& obj)
+{
+    return obj.scan(is);
+}
 
 class Student: public Human {
     string speciality;
@@ -129,19 +182,70 @@ public:
         set_group(group);
         set_rating(rating);
         set_attending(attending);
-        cout << "Student Constructor: \t" << this << endl;
+      //  cout << "Student Constructor: \t" << this << endl;
     }
     ~Student()
     {
-        cout << "Student Destructor: \t" << this << endl;
+      //  cout << "Student Destructor: \t" << this << endl;
     }
-    void print()const
+//    void print()const
+//    {
+//        Human::print();
+//        cout << "\nSpeciality: " << speciality << endl;
+//        cout << "\nFaculty: " << group << endl;
+//        cout << "\nRating: " << rating << endl;
+//        cout << "\nAttending: " << attending << endl;
+//    }
+    
+    std::ostream& print(std::ostream& os)const
     {
-        Human::print();
-        cout << "\nSpeciality: " << speciality << endl;
-        cout << "\nFaculty: " << group << endl;
-        cout << "\nRating: " << rating << endl;
-        cout << "\nAttending: " << attending << endl;
+        Human::print(os);
+        os.width(8);
+        os << std::left;
+        os << "\nSpeciality: " << speciality;
+        os.width(3);
+        os << "\nFaculty:    " << group;
+        os.width(3);
+        os << "\nRating:     " << rating;
+        os.width(3);
+        os << "\nAttending:  " << attending;
+        return os;
+    }
+    std::ofstream& print(std::ofstream& os)const
+    {
+        Human::print(os);
+        os.width(10);
+        os << std::left;
+        os << speciality;
+        os.width(10);
+        os << group;
+        os.width(10);
+        os << rating;
+        os.width(10);
+        os << attending;
+        return os;
+    }
+    std::istream& input(std::istream& is)
+    {
+        Human::input(is);
+        cout << "\nSpeciality: ";
+        is >> speciality;
+        cout << "\nFaculty:    ";
+        is >> group;
+        cout << "\nRating:     ";
+        is >> rating;
+        cout << "\nAttending:  ";
+        is >> attending;
+        return is;
+    }
+    std::ifstream& scan(std::ifstream& is)
+    {
+        Human::scan(is);
+        is >> speciality;
+        is >> group;
+        is >> rating;
+        is >> attending;
+        return is;
     }
 };
 
@@ -183,20 +287,65 @@ public:
         set_speciality(speciality);
         set_experience(experience);
         set_salary(salary);
-        cout << "Teacher constructor: \t" << this << endl;
+      //  cout << "Teacher constructor: \t" << this << endl;
     }
     ~Teacher()
     {
-        cout << "Teacher Destructor: \t" << this << endl;
+      //  cout << "Teacher Destructor: \t" << this << endl;
     }
     // Methods:
     
-    void print() const
+//    void print() const
+//    {
+//        Human::print();
+//        cout << "Speciality: " << speciality << endl;
+//        cout << "Working experience: " << experience << endl;
+//        cout << "Salary: " << salary << endl;
+//    }
+    
+    std::ostream& print(std::ostream& os)const
     {
-        Human::print();
-        cout << "Speciality: " << speciality << endl;
-        cout << "Working experience: " << experience << endl;
-        cout << "Salary: " << salary << endl;
+        Human::print(os);
+        os.width(8);
+        os << std::left;
+        os << "\nSpeciality:            " << speciality;
+        os.width(3);
+        os << "\nWorking experience:    " << experience;
+        os.width(3);
+        os << "\nSalary:                " << salary;
+        return os;
+    }
+    
+    std::ofstream& print(std::ofstream& os)const
+    {
+        Human::print(os);
+        os.width(8);
+        os << std::left;
+        os << speciality;
+        os.width(3);
+        os << experience;
+        os.width(3);
+        os << salary;
+        return os;
+    }
+    std::istream& input(std::istream& is)
+    {
+        Human::input(is);
+        cout << "\nSpeciality:          ";
+        is >> speciality;
+        cout << "\nWorking experience:  ";
+        is >> experience;
+        cout << "\nSalary:              ";
+        is >> salary;
+        return is;
+    }
+    std::ifstream& scan(std::ifstream& is)
+    {
+        Human::scan(is);
+        is >> speciality;
+        is >> experience;
+        is >> salary;
+        return is;
     }
     
 };
@@ -240,37 +389,113 @@ public:
         set_position(position);
         set_experience(experience);
         set_salary(salary);
-        cout << "Employee constructor: \t" << this << endl;
+      //  cout << "Employee constructor: \t" << this << endl;
     }
     ~Employee()
     {
-        cout << "Employee Destructor: \t" << this << endl;
+      //  cout << "Employee Destructor: \t" << this << endl;
     }
     // Methods:
     
-    void print() const
+//    void print() const
+//    {
+//        Human::print();
+//        cout << "Position: " << position << endl;
+//        cout << "Working experience: " << experience << endl;
+//        cout << "Salary: " << salary << endl;
+//    }
+    std::ostream& print(std::ostream& os)const
     {
-        Human::print();
-        cout << "Position: " << position << endl;
-        cout << "Working experience: " << experience << endl;
-        cout << "Salary: " << salary << endl;
+        Human::print(os);
+        os.width(8);
+        os << std::left;
+        os << "\nPosition:            " << position;
+        os.width(3);
+        os << "\nWorking experience:    " << experience;
+        os.width(3);
+        os << "\nSalary:                " << salary;
+        return os;
+    }
+    std::ofstream& print(std::ofstream& os)const
+    {
+        Human::print(os);
+        os.width(8);
+        os << std::left;
+        os << position;
+        os.width(3);
+        os << experience;
+        os.width(3);
+        os << salary;
+        return os;
+    }
+    std::istream& input(std::istream& is)
+    {
+        Human::input(is);
+        cout << "\nPosition:          ";
+        is >> position;
+        cout << "\nWorking experience:  ";
+        is >> experience;
+        cout << "\nSalary:              ";
+        is >> salary;
+        return is;
+    }
+    std::ifstream& scan(std::ifstream& is)
+    {
+        Human::scan(is);
+        is >> position;
+        is >> experience;
+        is >> salary;
+        return is;
     }
 };
+ 
+//#define SAVE_TO_FILE
+#define RECORD_TO_FILE
+
+Student* studentFaculty(const string& type)
+{
+    Student* student = nullptr;
+    if(type.find("faculty") != std::string::npos)
+    {
+        student = new Student("", "", 0, "", "", 0, 0);
+    }
+    return student;
+}
+Teacher* teacherDepartment(const string& type)
+{
+    Teacher* teacher = nullptr;
+    if(type.find("department") != std::string::npos)
+    {
+        teacher = new Teacher("", "", 0, "", 0, 0);
+    }
+    return teacher;
+}
+Employee* staffDepartment(const string& type)
+{
+    Employee* staff = nullptr;
+    if(type.find("staff") != std::string::npos)
+    {
+        staff = new Employee("", "", 0, "", 0, 0);
+    }
+    return staff;
+}
 
 int main(int argc, const char * argv[])
 {
+#ifdef SAVE_TO_FILE
+    
     Student* faculty[] =
     {
         new Student("Harry", "Potter", 15, "wizard", "Griffindor", 5, 100),
-        new Student("Ronald", "Weaseley", 15, "wizard", "Griffindor", 5, 100),
-        new Student("Hermione", "Granger", 15, "wizard", "Griggindor", 5, 100),
+        //new Student("Ronald", "Weaseley", 15, "wizard", "Griffindor", 5, 100),
+        //new Student("Hermione", "Granger", 15, "wizard", "Griffindor", 5, 100),
     };
     
     Teacher* department[] =
     {
         new Teacher("Severus", "Snape", 37, "potion-making", 14, 1587),
-        new Teacher("Alastor", "Moody", 56, "defence-against-art-Dark", 1, 2600),
-        new Teacher("Minerva", "McGonagall", 54, "transfiguration", 30, 3500),
+        //new Teacher("Alastor", "Moody", 56, "defence-against-art-Dark", 1, 2600),
+        //new Teacher("Minerva", "McGonagall", 54, "transfiguration", 30, 3500),
     };
     
     Employee* staff[] =
@@ -281,23 +506,48 @@ int main(int argc, const char * argv[])
     unsigned int total_salary = 0;
     unsigned int total_experience = 0;
     
+    // INPUT
+    
     for (int i = 0; i < sizeof(faculty) / sizeof(Student*) ; ++i)
     {
         cout << "\n _________________________ \n";
-        faculty[i] -> print();
+        //cout << *faculty[i];
+        cin >> *faculty[i];
     }
     
     for (int i = 0; i < sizeof(department)/ sizeof(Teacher*); ++i)
     {
         cout << "\n _________________________ \n";
-        department[i] -> print();
+        //cout << *department[i];
+        cin >> *department[i];
+        //total_salary += department[i] -> get_salary();
+    }
+    
+    for (int i = 0; i < sizeof(staff)/ sizeof(Employee*); ++i)
+    {
+        cout << "\n _________________________ \n";
+        //cout << *staff[i];
+        cin >> *staff[i];
+    }
+    
+    // OUTPUT
+    for (int i = 0; i < sizeof(faculty) / sizeof(Student*) ; ++i)
+    {
+        cout << "\n _________________________ \n";
+        cout << *faculty[i];
+    }
+    
+    for (int i = 0; i < sizeof(department)/ sizeof(Teacher*); ++i)
+    {
+        cout << "\n _________________________ \n";
+        cout << *department[i];
         total_salary += department[i] -> get_salary();
     }
     
     for (int i = 0; i < sizeof(staff)/ sizeof(Employee*); ++i)
     {
         cout << "\n _________________________ \n";
-        staff[i] -> print();
+        cout << *staff[i];
     }
     
     total_salary += staff[0] -> get_salary();
@@ -307,6 +557,41 @@ int main(int argc, const char * argv[])
     cout << "Total salary: " << total_salary << endl;
     cout << "Total experience: " << total_experience << endl;
     cout << "\n _________________________ \n";
+    
+    // OUTPUT IN FILE
+    
+    std::ofstream fout ("/Users/antonkurin/Documents/Cplusplus/InheritanceCplusplus/file.txt");
+    
+    for (int i = 0; i < sizeof(faculty) / sizeof(Student*) ; ++i)
+    {
+        fout.width(10);
+        fout << std::left;
+        fout << string(typeid(*faculty[i]).name()) + ":";
+        fout << *faculty[i];
+    }
+    
+    for (int i = 0; i < sizeof(department)/ sizeof(Teacher*); ++i)
+    {
+        fout.width(10);
+        fout << std::left;
+        fout << string(typeid(*department[i]).name()) + ":";
+        fout << *department[i];
+        total_salary += department[i] -> get_salary();
+    }
+    
+    for (int i = 0; i < sizeof(staff)/ sizeof(Employee*); ++i)
+    {
+        fout.width(10);
+        fout << std::left;
+        fout << string(typeid(*staff[i]).name()) + ":";
+        fout << *staff[i];
+    }
+    fout.close();
+    
+    system("more /Users/antonkurin/Documents/Cplusplus/InheritanceCplusplus/file.txt");
+    
+    
+    // CLEAR MEMORY
     
     for (int i = 0; i < sizeof(faculty) / sizeof(Student*) ; ++i)
     {
@@ -323,5 +608,55 @@ int main(int argc, const char * argv[])
         delete staff[i];
     }
     
+#endif // SAVE_TO_FILE
+    
+#ifdef RECORD_TO_FILE
+    
+    Student** faculty = nullptr;
+    int n = 0;
+    std::ifstream fin("/Users/antonkurin/Documents/Cplusplus/InheritanceCplusplus/file.txt");
+    
+    if(fin.is_open())
+    {
+        string student_type;
+        for(; !fin.eof(); n++)
+        {
+            getline(fin, student_type);
+        }
+        
+        faculty = new Student*[n] {};
+        
+        fin.clear();
+        fin.seekg(0);
+        
+        for(int i = 0; i < n; ++i)
+        {
+            getline(fin, student_type, ':');
+            faculty[i] = studentFaculty(student_type);
+            fin >> *faculty[i];
+        }
+    }
+    else
+    {
+        std::cerr << "Error: file is not found" << endl;
+    }
+    
+    for(int i = 0; i < n; ++i)
+    {
+        cout << *faculty[i] << endl;
+    }
+    
+    for (int i = 0; i < n; ++i)
+    {
+        delete faculty[i];
+    }
+    
+    delete [] faculty;
+    fin.close();
+    
+ 
+#endif // RECOD_TO_FILE
+    
     return 0;
+    
 }
